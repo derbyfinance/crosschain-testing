94afb5714429fc36641bc94e35f83349e3771fd7
DolanPump <nickvanrossenberg@hotmail.com>
2023-02-24 10:13:56 +0100

    init

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,86 +1,9 @@
-import * as dotenv from "dotenv";
-
-import { HardhatUserConfig, task } from "hardhat/config";
-// import "hardhat-gas-reporter";
-import "solidity-coverage";
-import "@typechain/hardhat";
-import "@nomiclabs/hardhat-ethers";
-import "@nomiclabs/hardhat-waffle";
-import "@nomiclabs/hardhat-etherscan";
-
-dotenv.config();
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+import 'hardhat-deploy';
 
 const config: HardhatUserConfig = {
-  solidity: {
-    version: "0.8.11",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 10,
-      },
-    },
-  },
-  networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
-    rinkeby: {
-      url: process.env.RINKEBY_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
-    goerli: {
-      url: process.env.GOERLI_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
-    bsc: {
-      url: process.env.BSC_TESTNET_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
-    mumbai: {
-      url: process.env.MUMBAI_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
-    hardhat: {
-      forking: {
-        url: `${process.env.PROVIDER_FORKING}`,
-        blockNumber: 14748903,
-      },
-    },
-  },
-  // gasReporter: {
-  //   enabled: process.env.REPORT_GAS ? true : false,
-  //   currency: "USD",
-  //   coinmarketcap: process.env.CMC,
-  // },
-  etherscan: {
-    apiKey: {
-      rinkeby: process.env.ETHERSCAN_API_KEY as string,
-      bscTestnet: process.env.BINANCESCAN_API_KEY as string,
-      polygonMumbai: process.env.POLYGONSCAN_API_KEY as string
-    }
-  },
-  mocha: {
-    timeout: 3000000,
-  },
+  solidity: '0.8.17',
 };
 
 export default config;
